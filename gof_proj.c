8adf61af1361a897342d3fca0d4fee581bb13a06
MarceauFillon <marceau-fillon@outlook.com>
2018-10-17 16:29:22 +0200

    Previous code commented, playing with ncurses to learn how to use it

--- gof_proj.c
+++ gof_proj.c
@@ -1,28 +1,94 @@
 #include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <menu.h>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 	4
+
+char *choices[] = {
+                        "The R-Pentomino",
+                        "Diehard",
+                        "Acorn",
+                        "Exit",
+                  };
 
 // We need to define what are the parameters of our struct
 typedef struct cells {
 	int alive;
 }cell;
 
-void display(WINDOW *win, cell **area, int startx, int starty, int endx, int endy)
-{	int i, j;
-	wclear(win);
-	for(i = startx; i <= endx; ++i)
-		for(j = starty;j <= endy; ++j)
-			if(area[i][j].alive == 1)
-				mvwaddch(win, j, i, '*');
-	wrefresh(win);
-}
-
 int main()
 {
+	char welcomeMessage[]="Welcome to the Game Of Life";		/* message to be appeared on the screen */
+	char namesMessage[]="By blabla1 blabla2 blabla3 blabla4";
 
+	int inic_x =0;
+	int inic_y =0;
+	int final_x =0;
+	int final_y =0;				/* to store the number of rows and *
+						 * the number of colums of the screen */
 
-	initscr(); // Start curses mode
+	ITEM **my_items;
+	int c;				
+	MENU *my_menu;
+	int n_choices, i;
+	ITEM *cur_item;
+
+	initscr();				/* start the curses mode */
+	getmaxyx(stdscr,final_x,final_y);		/* get the number of rows and columns */
+
+	//First screen *******************************************************
+	mvprintw(final_x/2,(final_y-strlen(welcomeMessage))/2,"%s",welcomeMessage);
+	                                	/* print the message at the center of the screen */
+	mvprintw(final_x/2 +1,(final_y-strlen(namesMessage))/2,"%s",namesMessage);
+
+	refresh();
+	getch();
+	//********************************************************************
+	erase();
+	//Second screen *****************************************************
+	//Just waiting for user to press something
+	getch();
+
 	cbreak();
+	noecho();
+	keypad(stdscr, TRUE);
+		
+	n_choices = ARRAY_SIZE(choices);
+	my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+
+	for(i = 0; i < n_choices; ++i)
+		my_items[i] = new_item(choices[i], choices[i]);
+	my_items[n_choices] = (ITEM *)NULL;
+
+	my_menu = new_menu((ITEM **)my_items);
+	mvprintw(LINES - 2, 0, "F4 to Exit");
+	post_menu(my_menu);
+	refresh();
+
+	while((c = getch()) != KEY_F(4))
+	{   switch(c)
+	    {	case KEY_DOWN:
+		        menu_driver(my_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(my_menu, REQ_UP_ITEM);
+				break;
+		}
+	}	
+
+	free_item(my_items[0]);
+	free_item(my_items[1]);
+	free_menu(my_menu);
+	endwin();
+ 
+ endwin();
+
+ return 0;
+	/*initscr(); // Start curses mode
+	raw();
 	keypad(stdscr, TRUE); //Enable keys
 
 	int inic_x=0; //first position of the screen
@@ -30,7 +96,7 @@ int main()
 	int inic_y=0;
 	int final_y=LINES;// the screen sizes after initscr()
 
-
+	char answer = '0';
 
 	// Allocating memory for a 2D array of cell type
 	// Array of pointers
@@ -44,22 +110,23 @@ int main()
 	for (i = 0; i < fsize; ++i)
 		field[i] = (cell *)  malloc(fsize * sizeof(cell ));
 
-	printw("Please indicate the intial configuration you would like to effect\n");
-	printw("L :1");
-	raw();
-	char answer = getch();
-	printw("Answer is %c \n", answer);
+	printf("Please indicate the intial configuration you would like to effect\n");
+	printf("L :1\n");
+	//raw();
+	scanf("%c",&answer);
+	printf("Answer is %c \n", answer);
 
-	if(answer=='1')
+	/*if(*answer=='1')
 	{
 		field[0][0].alive = 1;
 		field[1][0].alive = 1;
 		field[2][0].alive = 1;
-	}
+	}*/
 
-	display(stdscr, field, inic_x, inic_y, final_x, final_y);
+	//displayGrid(stdscr, field, inic_x, inic_y, final_x, final_y);
 // Game: we need to update all the cells until the user press F1
 
+/*
 while(getch() != KEY_F(1))
 	{	
 		for(i = inic_x; i <= final_x; ++i)
@@ -77,8 +144,7 @@ while(getch() != KEY_F(1))
 				*/
 				// Uptade the screen with the necessary changes 
 			}	
-		}		
-	}
+
 /* To update the screen we need create a function that uses:
 mvaddch(row,col,ch) -> is used to move the cursor to a given point, and then print
 refresh();	-> Print it on to the real screen 
@@ -86,7 +152,9 @@ refresh();	-> Print it on to the real screen
 
 */
 
+/*
 for (i = 0; i < fsize; ++i)
 	free(field[i]); //  free each array of cells d
 free(field); // free the array of pointers field
-}
+*/
+
